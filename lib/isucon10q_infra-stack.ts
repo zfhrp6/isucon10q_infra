a38513ead73ddab7b9b2e68418b0df4fe52141e4
zfhrp <zfhrp6@users.noreply.github.com>
2020-11-09 21:14:12 +0900

    update dpes

--- lib/isucon10q_infra-stack.ts
+++ lib/isucon10q_infra-stack.ts
@@ -1,9 +1,9 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
 
 export class Isucon10QInfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
   }
 }
